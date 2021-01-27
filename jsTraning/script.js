a0d32424ca9e3bb2a97daf8a649ddc4ad8753b56
alexsander <as.shnyrenkov@mail.ru>
2021-01-27 20:48:19 +0300

    conditions

--- jsTraning/script.js
+++ jsTraning/script.js
@@ -1,20 +1,4 @@
 'use strict';
 
-const numberOfFilms = prompt('сколько фильмов вы смотрели');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 
